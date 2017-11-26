a8956addaa1f44aab01e4f3d1053076071f75d81
manudelca <32716242+manudelca@users.noreply.github.com>
2017-11-26 16:40:36 -0300

    Update grafo.py
    Correcciones varias. Obtener_aristas_v

--- grafo.py
+++ grafo.py
@@ -18,39 +18,39 @@ class Grafo:
         self.dicc={}
 
     def agregar_vertice(self,vertice):
-        '''Agrega el vertice recibido al grafo.'''  #No se pueden repetir vertices???creo que con eso lo arregle
-        if vertice not in self.dicc:
-            self.dicc[vertice]={}
-        #tendriamos que tirar una error en caso de else?
+        '''Agrega el vertice recibido al grafo.'''
+         if vertice not in self.dicc:
++            self.dicc[vertice]={}
++        #tendriamos que tirar una error en caso de else?
 
     def borrar_vertice(self,vertice):
         '''Borra el vertice del grafo. Levanta un error si no se encuentra'''
         if not vertice in self.dicc:
-            raise ValueError('El vertice no se encuentra en el grafo')#Que error levanto??
+            raise ValueError('El vertice no se encuentra en el grafo')
         for adyacente in self.dicc[vertice]:
             (self.dicc[adyacente]).pop(vertice)
 
     def agregar_arista(self, v1, v2, info=None):
         '''Recibe los dos vertices a unir y la info adicional de la arista (None por defecto)
         y los une. Si los vertices no se encuentran en el grafo devuelve error.'''
-        if not v1 in self.dicc:
-            raise ValueError('El vertice {} no se encuentra en el grafo'.format(v1))
-        if not v2 in self.dicc:
-            raise ValueError('El vertice {} no se encuentra en el grafo'.format(v2))
+        if not v1 in self.dicc or not v2 in self.dicc:
+            raise ValueError('El o los vertices no se ecuentran')
         arista=_Arista(v1,v2,info)
         lista1=(self.dicc[v1]).get(v2,[])
+        if not lista1:
+            self.dicc[v1][v2]=lista1
         lista1.append(arista)
-        lista2=(self.dicc[v2]).get(v1,[])
+        lista2=(self.dicc[v2]).get(v1,[]):
+        if not lista2:
+            self.dicc[v2][v1]=lista2
         lista2.append(arista)
 
-    def borrar_arista(self,v1,v2):
+    def borrar_arista(self,v1,v2,info):
         '''Recibe dos vertices y elimina la arista entre ambos. Devuelve una tupla
         con una tupla representando una arista y la info de la arista ((v1,v2),info).
         Devuelve error en caso de no encontrarse'''
-        if not v1 in self.dicc:
-            raise ValueError('El vertice {} no se encuentra en el grafo'.format(v1))
-        if not v2 in self.dicc:
-            raise ValueError('El vertice {} no se encuentra en el grafo'.format(v2))
+        if not v1 in self.dicc or not v2 in self.dicc:
+            raise ValueError('El o los vertices no se ecuentran')
         arista=(self.dicc[v1]).get(v2,None)
         if not arista:
             raise ValueError('La arista no existe')
@@ -68,11 +68,9 @@ class Grafo:
     def es_adyacente(self,v1,v2):
         '''Recibe dos vertices y devuelve True en caso de que sean adyacentes.
         False en caso de que no. Devuelve un error en caso de no encontrarse el vertice.'''
-        if not v1 in self.dicc:
-            raise ValueError('El vertice no se encuentra en el grafo')
-        if not v2 in self.dicc:
-            raise ValueError('El vertice no se encuentra en el grafo')
-        return v2 in self.dicc[v1]
+        if not v1 in self.dicc or not v2 in self.dicc:
+            raise ValueError('El o los vertices no se ecuentran')
+        return v2 in self.dicc[v1] and v1 in self.dicc[v2]
 
     def obtener_vertices(self):
         '''Devuelve una lista con los vertices del grafo'''
@@ -84,57 +82,30 @@ class Grafo:
         for v in self.dicc:
             for adyacente in self.dicc[v]:
                 lista_aristas=self.dicc[v][adyacente]
-                for elem in lista_aristas:
-                    arista_l=(elem.arista,elem.info)
+                for arista in lista_aristas:
+                    arista_l=(arista.arista,arista.info)
                     if  not arista_l in aristas:
                         aristas[arista_l]=True
         return list(aristas)
 
     def obtener_arista(self,v1,v2):
-        '''Recibe dos vertices y devuelve una lista con las aristas que los une'''
-        #Hay que controlar los errores
+        '''Recibe dos vertices y devuelve una lista con las aristas que los une.
+        En caso de que no las haya, devuelve una lista vacia'''
+        if not v1 in self.dicc or not v2 in self.dicc:
+            raise ValueError('El o los vertices no se ecuentran')
         resultado=[]
-        for arista in self.dicc[v1][v2]:
-            resultado.append(arista.arista,arista.info)
+        aristas=(self.dicc[v1]).get(v2,[])
+        for arista in aristas:
+            resultado.append((arista.arista,arista.info))
         return resultado
 
-#def bfs(grafo,origen):
-#    '''Recibe un grafo y un origen y recorre por capas el grafo. Devuelve un
-#    diccionario con los padres de cada vertice y otro con el orden de cada vertice.'''
-#    visitados={}
-#    padre={}
-#    orden={}
-#    q=collections.deque()
-#    q.append(origen)
-#    visitados[origen]=True
-#    orden[origen]=0
-#    padre[origen]=None
-#    while len(q):
-#        v=q.popleft()
-#        for w in grafo.obtener_adyacentes(v):
-#            if not w in visitados:
-#                visitados[w]=True
-#                padre[w]=v
-#                orden[w]=orden[v]+1
-#                q.append(w)
-#    return padre,orden
-
-#def dfs(grafo,origen):
-#    '''Recibe un grafo y un origen y recorre por profundidad el grafo. Devuelve
-#    un diccionario con los padres de cada de vertice y otro con el orden de cada
-#    vertice.'''
-#    visitados={}
-#    padre={}
-#    orden={}
-#    dfs_visitar(grafo,origen,visitados,padre,orden)
-#    return padre,orden
-
-
-#def dfs_visitar(grafo,v,visitados,padre,orden):
-#    '''Funcion recursiva de dfs'''
-#    visitados[v]=True
-#    for w in grafo.obtener_adyacentes(v):
-#        if w not in visitados:
-#            padre[w]=v
-#            orden[v]=orden.get(v,0)+1
-#            dfs_visitar(grafo,w,visitados,padre,orden)
+    def obtener_aristas_v(self,vertice):
+        '''Recibe un vertice y devuelve una lista con todas las aristas
+        conectadas al mismo.'''
+        if not vertice in self.dicc:
+            raise ValueError('El vertice no se encuentra en el grafo')
+        resultado=[]
+        for w,aristas in (self.dicc[vertice]).items():
+            for arista in aristas:
+                resultado.append((arista.arista,arista.info))
+        return resultado
